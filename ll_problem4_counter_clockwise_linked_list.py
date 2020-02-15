339f2f35d1d76a06941ac29f251cb391ae4887ac
RaymondDashWu <raymond31670@hotmail.com>
2020-02-15 15:00:29 -0800

    [ADD] Solution problem4 + template problem 5

--- ll_problem4_counter_clockwise_linked_list.py
+++ ll_problem4_counter_clockwise_linked_list.py
@@ -18,9 +18,91 @@
 # LL size 3, k is 1
 
 # PSEUDO BRAINSTORM 1
-# iterate through nodes + have a previous
-# set previous.next to node.next
+# A -> B -> C , k = 1
+# keep track of number of node traversals
+# keep track of original_head
+
+# when node.next is none change modified head to point to k + 1 node
+# tail points to original head
+# change modified head to self.head
 
 # PSEUDO BRAINSTORM 2
 # put all data into an array
 # iterate through the array at [::-1] and create a new linked list
+
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = None
+
+    def print_ll(self):
+        node = self.head
+        while node is not None:
+            print(node.data)
+            node = node.next
+# A -> B -> C , k = 1
+# keep track of number of node traversals
+# keep track of original_head
+
+# when node.next is none change modified head to point to k + 1 node
+# tail points to original head
+# change modified head to self.head
+    def rotate_counter_clockwise(self, k):
+        traversal_counter = 0
+        original_head = self.head
+        node = self.head
+
+        # traverses up until k and splits it into 2 linked lists in memory. One starting with 
+        # modified head & original head
+        # ex: A → B → C → D → E → F, k = 4
+        # modified head E → F
+        # original head A → B → C → D
+        while node is not None:
+            if traversal_counter == k:
+                modified_head = node
+                break
+
+            prev = node
+            node = node.next
+            traversal_counter += 1
+
+        # splits into 2 LL and then sets the end point of the original_head
+        prev.next = None
+
+        # continued traversal through modified_head LL
+        while node is not None:
+            prev = node
+            node = node.next
+        prev.next = original_head
+        self.head = modified_head
+        
+
+if __name__ == "__main__":
+    ll = LinkedList()
+
+    ll.head = Node("A")
+    
+    second = Node("B")
+    ll.head.next = second
+    
+    third = Node("C")
+    second.next = third
+
+    fourth = Node("D")
+    third.next = fourth
+
+    fifth = Node("E")
+    fourth.next = fifth
+
+    sixth = Node("F")
+    fifth.next = sixth
+    
+    ll.rotate_counter_clockwise(4)
+    print(ll.print_ll())
+    print("Head of LL", ll.head.data)
---
+++ ll_problem5_sort_k_linked_lists.py
@@ -0,0 +1,1 @@
+# Given an array of k linked lists, each of whose items are in sorted order, combine all nodes (do not create new nodes) into a single linked list with all items in order.
