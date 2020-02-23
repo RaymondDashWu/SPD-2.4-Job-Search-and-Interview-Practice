7e35f4d4c99a699080c6e7ba8d27159caf802d78
RaymondDashWu <raymond31670@hotmail.com>
2020-02-22 22:37:12 -0800

    Night cap

--- ll_problem5_sort_k_linked_lists.py
+++ ll_problem5_sort_k_linked_lists.py
@@ -1,1 +1,92 @@
 # Given an array of k linked lists, each of whose items are in sorted order, combine all nodes (do not create new nodes) into a single linked list with all items in order.
+
+# LL1 1 → 4 → 10
+# LL2 5 → 8 → 11
+
+# Expected 1 → 4 → 5 → 8 → 10 → 11
+
+# PSEUDO BRAINSTORM
+# have 3 sets of pointers per ll (total 6)
+# 1 pointer for prev, curr, next
+# compare curr pointers and use prev pointers to change next
+
+# NOTE Unfinished
+
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = None
+
+    def print_ll(self):
+        node = self.head
+
+        while node is not None:
+            print(node.data)
+            node = node.next
+
+    def compare_nodes(self, node):
+        pass
+
+    def sort_ll(self, ll2):
+        ll1_prev = None
+        ll2_prev = None
+        ll1_curr = self.head
+        ll2_curr = ll2.head
+        ll1_next = ll1_curr.next
+        ll2_next = ll2_curr.next
+
+
+# LL1 1 → 4 → 10
+# LL2 5 → 8 → 11
+
+        # while neither are none
+        while (ll1_curr, ll2_curr) is not None:
+            ll2_prev = ll2_curr
+            ll1_prev = ll1_curr
+            # moving comparison section
+            while ll1_curr.data > ll2_curr.data:
+                ll2_curr = ll2_curr.next
+                ll2_next = ll2_next.next
+                if ll1_curr.data > ll2_curr.data:
+                    ll2_curr.next = ll1_prev
+            while ll2_curr.data > ll1_curr.data:
+                ll1_curr = ll1_curr.next
+                ll1_next = ll1_next.next
+                if ll2_curr.data > ll1_curr.data:
+                    ll1_curr.next = ll2_prev
+            # changing pointer section
+
+        # compare curr pointers
+        # change pointers with prev
+        # ll that is higher gets moved up one
+
+if __name__ == "__main__":
+    ll = LinkedList()
+
+    ll.head = Node(1)
+    
+    second = Node(4)
+    ll.head.next = second
+    
+    third = Node(10)
+    second.next = third
+
+    ll2 = LinkedList()
+
+    ll2.head = Node(5)
+
+    fourth = Node(8)
+    ll2.head.next = fourth
+
+    fifth = Node(11)
+    fourth.next = fifth
+    
+    ll.sort_ll(ll2)
+    print("ll", ll.print_ll())
+    print("Head of LL", ll.head.data)
--- recursion_problem3_t9_keypad_combination.py
+++ recursion_problem3_t9_keypad_combination.py
@@ -9,6 +9,14 @@
 # dictionary of mappings given
 
 # PSEUDO BRAINSTORM
+# base case when there is no next digit in first number of string. SCRATCHED for len of array
+
+# initialize variable on empty string. To be used to combine letters later
+# keep track of 2 indexes
+# first one will be per number
+# 2nd one will be letter per letter at number
+# recursively call after number is iterated through at letter + 1
+# append combined letter string to output? Unsure of order
 
 t9_keypad_mappings = {
     2: ("a", "b", "c"),
@@ -21,23 +29,53 @@ t9_keypad_mappings = {
     9: ("w", "x", "y", "z"),
 }
 
-def valid_t9_conversion(str_numbers):
+def valid_t9_conversion_and_length(str_numbers):
     """Used to check if the numbers being input are valid for a t9 conversion. 
-    Ex: 0, 1, symbols, letters are not allowed"""
+    Ex: 0, 1, symbols, letters are not allowed
+    
+    Added length calculation since we're looping through anyways. To be used in t9_letters function as
+    part of the base case. Will calculate the total length the output should be"""
     valid_numbers = ["2","3","4","5","6","7","8","9"]
-
+    total_len = 0
     for num in str_numbers:
         if num in valid_numbers:
-            continue
+            total_len += len(t9_keypad_mappings[int(num)])
         else:
             return ValueError
+    return total_len
 
-def t9_letters(str_numbers, valid = False):
-    if valid == False:
-        valid_t9_conversion(str_numbers)
+
+def t9_letters(str_numbers, valid = False, lett_ind = 0, len_total = 0, output_arr = []):
+    if valid == False: # First check
+        # From earlier valid_t9_conversion_and_length fn.
+        # Calculates how long the array should be.
+        # Ex: if string is "23" len of 2 & 3 at t9_keypad_mappings is 3 * 3 = 9. Total array length should be 9
+        len_total = valid_t9_conversion_and_length(str_numbers) 
         valid = True
+    # Base case
+    if len(output_arr) == len_total: 
+        return output_arr
+
+    # [2,3]
+    split_str = [int(num) for num in str_numbers]
+
+# initialize variable on empty string. To be used to combine letters later
+# keep track of 2 indexes
+# first one will be per number
+# 2nd one will be letter per letter at number
+# recursively call after number is iterated through at letter + 1
+# append combined letter string to output? Unsure of order
+# print(t9_keypad_mappings[[2][0]][0])
+
+    for num_ind in range(len(split_str)):
+        append_to_arr = ""
+        print(t9_keypad_mappings[split_str[num_ind]][lett_ind])
+        append_to_arr += t9_keypad_mappings[split_str[num_ind]][lett_ind]
+        t9_letters(str_numbers, valid = valid, lett_ind = lett_ind + 1, len_total = len_total, output_arr = output_arr)
+        output_arr.append(append_to_arr)
     # pass
 
 if __name__ == "__main__":
+    print(t9_letters("23"))
     assert t9_letters("23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
-    print(t9_letters("4663"))
+    # print(t9_letters("4663"))
