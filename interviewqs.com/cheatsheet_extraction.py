0bc5cfcd98815c80ecf5214ad751097e4740d1ab
RaymondDashWu <raymond31670@hotmail.com>
2020-05-20 17:35:21 -0700

    [ADD] retention analysis template

--- interviewqs.com/cheatsheet_extraction.py
+++ interviewqs.com/cheatsheet_extraction.py
@@ -19,84 +19,73 @@ def extract_sql(corpus):
     return set(re.findall('[A-Z]{2,}', corpus))
 
 corpus = """
-AND Operator
-The AND operator allows multiple conditions to be combined. Records must match both conditions that are joined by AND to be included in the result set. The given query will match any car that is blue and made after 2014.
+Aggregate Functions
+Multiple Tables
+Column References
+The GROUP BY and ORDER BY clauses can reference the selected columns by number in which they appear in the SELECT statement. The example query will count the number of movies per rating, and will:
 
-SELECT model 
-FROM cars 
-WHERE color = 'blue' 
-  AND year > 2014;
-AS Clause
-Columns or tables can be aliased using the AS clause. This allows columns or tables to be specifically renamed in the returned result set. The given query will return a result set with the column for name renamed to movie_title.
+GROUP BY column 2 (rating)
+ORDER BY column 1 (total_movies)
+SELECT COUNT(*) AS 'total_movies', 
+   rating 
+FROM movies 
+GROUP BY 2 
+ORDER BY 1;
+SUM() Aggregate Function
+The SUM() aggregate function takes the name of a column as an argument and returns the sum of all the value in that column.
 
-SELECT name AS 'movie_title'
-FROM movies;
-OR Operator
-The OR operator allows multiple conditions to be combined. Records matching either condition joined by the OR are included in the result set. The given query will match customers whose state is either 'CA' or 'NY'.
+SELECT SUM(salary)
+FROM salary_disbursement;
+MAX() Aggregate Function
+The MAX() aggregate function takes the name of a column as an argument and returns the largest value in a column. The given query will return the largest value from the amount column.
 
-SELECT name
-FROM customers 
-WHERE state = 'CA' 
-   OR state = 'NY';
-% Wildcard
-The % wildcard can be used in a LIKE operator pattern to match zero or more unspecified character(s). The given query will match any movie that begins with The, followed by zero or more of any characters.
+SELECT MAX(amount) 
+FROM transactions;
+COUNT() Aggregate Function
+The COUNT() aggregate function returns the total number of rows that match the specified criteria. For instance, to find the total number of employees who have less than 5 years of experience, the given query can be used.
 
-SELECT name
-FROM movies
-WHERE name LIKE 'The%';
-SELECT Statement
-The SELECT * statement returns all columns from the provided table in the result set. The given query will fetch all columns and records (rows) from the movies table.
+Note: A column name of the table can also be used instead of *. Unlike COUNT(*), this variation COUNT(column) will not count NULL values in that column.
 
-SELECT *
-FROM movies;
-_ Wildcard
-The _ wildcard can be used in a LIKE operator pattern to match any single unspecified character. The given query will match any movie which begins with a single character, followed by ove.
+SELECT COUNT(*)
+FROM employees
+WHERE experience < 5;
+GROUP BY Clause
+The GROUP BY clause will group records in a result set by identical values in one or more columns. It is often used in combination with aggregate functions to query information of similar records. The GROUP BY clause can come after FROM or WHERE but must come before any ORDER BY or LIMIT clause.
 
-SELECT name
-FROM movies
-WHERE name LIKE '_ove';
-ORDER BY Clause
-The ORDER BY clause can be used to sort the result set by a particular column either alphabetically or numerically. It can be ordered in two ways:
+The given query will count the number of movies per rating.
 
-DESC is a keyword used to sort the results in descending order.
-ASC is a keyword used to sort the results in ascending order (default).
-SELECT *
-FROM contacts
-ORDER BY birth_date DESC;
-LIKE Operator
-The LIKE operator can be used inside of a WHERE clause to match a specified pattern. The given query will match any movie that begins with Star in its title.
+SELECT rating, 
+   COUNT(*) 
+FROM movies 
+GROUP BY rating;
+MIN() Aggregate Function
+The MIN() aggregate function returns the smallest value in a column. For instance, to find the smallest value of the amount column from the table named transactions, the given query can be used.
 
-SELECT name
-FROM movies
-WHERE name LIKE 'Star%';
-DISTINCT Clause
-Unique values of a column can be selected using a DISTINCT query. For a table contact_details having five rows in which the city column contains Chicago, Madison, Boston, Madison, and Denver, the given query would return:
+SELECT MIN(amount) 
+FROM transactions;
+AVG() Aggregate Function
+The AVG() aggregate function returns the average value in a column. For instance, to find the average salary for the employees who have less than 5 years of experience, the given query can be used.
 
-Chicago
-Madison
-Boston
-Denver
-SELECT DISTINCT city
-FROM contact_details;
-BETWEEN Operator
-The BETWEEN operator can be used to filter by a range of values. The range of values can be text, numbers, or date data. The given query will match any movie made between the years 1980 and 1990, inclusive.
+SELECT AVG(salary)
+FROM employees
+WHERE experience < 5;
+HAVING Clause
+The HAVING clause is used to further filter the result set groups provided by the GROUP BY clause. HAVING is often used with aggregate functions to filter the result set groups based on an aggregate property. The given query will select only the records (rows) from only years where more than 5 movies were released per year.
 
-SELECT *
-FROM movies
-WHERE year BETWEEN 1980 AND 1990;
-LIMIT Clause
-The LIMIT clause is used to narrow, or limit, a result set to the specified number of rows. The given query will limit the result set to 5 rows.
+SELECT year, 
+   COUNT(*) 
+FROM movies 
+GROUP BY year
+HAVING COUNT(*) > 5;
+Aggregate Functions
+Aggregate functions perform a calculation on a set of values and return a single value:
 
-SELECT *
-FROM movies
-LIMIT 5;
-NULL Values
-Column values can be NULL, or have no value. These records can be matched (or not matched) using the IS NULL and IS NOT NULL operators in combination with the WHERE clause. The given query will match all addresses where the address has a value or is not NULL.
-
-SELECT address
-FROM records
-WHERE address IS NOT NULL;
-WHERE Clause
-The WHERE clause is used to filter records (rows) that match a certain condition. The given query will select all records where the pub_year equals 2017."""
+COUNT()
+SUM()
+MAX()
+MIN()
+AVG()
+ROUND() Function
+The ROUND() function will round a number value to a specified number of places. It takes two arguments: a number, and a number of decimal places. It can be combined with other aggregate functions, as shown in the given query. This query will calculate the average rating of movies from 2015, rounding to 2 decimal places."""
 
 print(extract_sql(corpus))
